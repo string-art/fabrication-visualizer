9ca22f244fa319ccdfedf93baa86e9a158edf6d0
rebeccayelin <rebeccalin@alumni.ubc.ca>
2023-10-21 15:16:42 -0400

    wip: setting interface to visualize project

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+// Dimensions (in cm)
+const n = 10;
+const width = 66.0;
+const spacing = 6.29;
+
 // Initialize Three.js
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+const fov = Math.atan(0.5) * 180 / Math.PI * 2;
+const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 1000);
 const renderer = new THREE.WebGLRenderer();
-
+renderer.setClearColor(0xffffff, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Create a box with five faces of nails
 const nails = [];
-const n = 10;
-const spacing = 6.29; // in cm
-const offset = 33.0;
+const offset = width / 2;
 const rotation = Math.PI / 2;
 nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, 0, new THREE.Vector3(0, 0, -offset)));           // back face
 nails.push(...createFaceOfNails(n, n, spacing, spacing, rotation, 0, new THREE.Vector3(0, -offset, 0)));    // bottom face
@@ -18,11 +22,11 @@ nails.push(...createFaceOfNails(n, n, spacing, spacing, rotation, 0, new THREE.V
 nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, rotation, new THREE.Vector3(-offset, 0, 0)));    // left face
 nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, rotation, new THREE.Vector3(offset, 0, 0)));     // right face
 
-// TODO: add labels to the nails
 function createFaceOfNails(nx, ny, dx, dy, rx, ry, offset) {
     const points = [];
     const startX = -(nx - 1) * dx / 2;
     const startY = -(ny - 1) * dy / 2;
+    // Create grid of points, row by row
     for (let y = 0; y < ny; y++) {
         for (let x = 0; x < nx; x++) {
             point = new THREE.Vector3(startX + x * dx, startY + y * dy, 0);
@@ -35,13 +39,28 @@ function createFaceOfNails(nx, ny, dx, dy, rx, ry, offset) {
     return points;
 }
 
+// TODO: test!
+function indexToLabel(nails, n, i) {
+    // Assume five faces of nails
+    const faceNum = Math.floor(i / (n * n));
+    const indexInFace = i % (n * n);
+    const row = Math.floor(indexInFace / n);
+    const col = indexInFace % n;
+    return `F${faceNum}(row ${row + 1}, col ${col + 1})`;
+}
+
+// TODO: new line every 10 labels
+function indicesToLabels(nails, n, indices) {
+    return indices.map(i => indexToLabel(nails, n, i));
+}
+
 // Add the nails to the scene
 const geometry = new THREE.BufferGeometry().setFromPoints(nails);
 const material = new THREE.PointsMaterial({ color: 0xff0000 });
 const pointsObject = new THREE.Points(geometry, material);
 scene.add(pointsObject);
 
-camera.position.z = 150;
+camera.position.z = width * 2;
 
 // Add an axes helper
 const axesHelper = new THREE.AxesHelper(5);
@@ -49,7 +68,7 @@ scene.add(axesHelper);
 
 // Render the scene
 function animate() {
-		requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
     // pointsObject.rotation.y += 0.005;
     renderer.render(scene, camera);
 }
