299a4ed5f623e738a251b64a548450e9f38b4c27
rebeccayelin <rebeccalin@alumni.ubc.ca>
2024-09-25 14:28:43 -0400

    understand field of view

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// View setup
-const imgWidth = 800;
-const imgHeight = 600;
-
-// Box dimensions (in cm)
+// Nail box (in cm)
 const n = 10;
-const width = 66.0;
+const cubeSize = 66.0;
 const spacing = 6.29;
+const offset = cubeSize / 2;
 
 const nails = [];
-const offset = width / 2;
 
-// Faces of nails
-nails.push(...createFaceOfNails(n, n, spacing, spacing, -Math.PI / 2, 0, new THREE.Vector3(0, offset, 0)));    // (F1) top face
-nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, Math.PI / 2, new THREE.Vector3(offset, 0, 0)));     // (F2) right face
-nails.push(...createFaceOfNails(n, n, spacing, spacing, Math.PI / 2, 0, new THREE.Vector3(0, -offset, 0)));    // (F3) bottom face
-nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, -Math.PI / 2, new THREE.Vector3(-offset, 0, 0)));   // (F4) left face
-nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, Math.PI, new THREE.Vector3(0, 0, -offset)));        // (F5) back face
+// Camera
+const imgWidth = 800;     // determines aspect ratio
+const imgHeight = 600;
 
-function createFaceOfNails(nx, ny, dx, dy, rx, ry, offset) {
-    const points = [];
-    const startX = -(nx - 1) * dx / 2;
-    const startY = -(ny - 1) * dy / 2;
-    // Create grid of points, row by row
-    for (let y = 0; y < ny; y++) {
-        for (let x = 0; x < nx; x++) {
-            let point = new THREE.Vector3(startX + x * dx, startY + y * dy, 0);
-            point.applyAxisAngle(new THREE.Vector3(1, 0, 0), rx);
-            point.applyAxisAngle(new THREE.Vector3(0, 1, 0), ry);
-            point.add(offset);
-            points.push(point);
+const distanceFromCube = 100.0;  // i.e., 1m away from box
+const verticalPadding = cubeSize / 2;
+
+// Create faces of nails
+{
+    nails.push(...createFaceOfNails(n, n, spacing, spacing, -Math.PI / 2, 0, new THREE.Vector3(0, offset, 0)));    // (F1) top face
+    nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, Math.PI / 2, new THREE.Vector3(offset, 0, 0)));     // (F2) right face
+    nails.push(...createFaceOfNails(n, n, spacing, spacing, Math.PI / 2, 0, new THREE.Vector3(0, -offset, 0)));    // (F3) bottom face
+    nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, -Math.PI / 2, new THREE.Vector3(-offset, 0, 0)));   // (F4) left face
+    nails.push(...createFaceOfNails(n, n, spacing, spacing, 0, Math.PI, new THREE.Vector3(0, 0, -offset)));        // (F5) back face
+
+    function createFaceOfNails(nx, ny, dx, dy, rx, ry, offset) {
+        const points = [];
+        const startX = -(nx - 1) * dx / 2;
+        const startY = -(ny - 1) * dy / 2;
+        // Create grid of points, row by row
+        for (let y = 0; y < ny; y++) {
+            for (let x = 0; x < nx; x++) {
+                let point = new THREE.Vector3(startX + x * dx, startY + y * dy, 0);
+                point.applyAxisAngle(new THREE.Vector3(1, 0, 0), rx);
+                point.applyAxisAngle(new THREE.Vector3(0, 1, 0), ry);
+                point.add(offset);
+                points.push(point);
+            }
         }
+        return points;
     }
-    return points;
 }
 
 // Set up
@@ -45,9 +49,9 @@ renderer.setSize(imgWidth, imgHeight);
 document.body.appendChild(renderer.domElement);
 
 // Camera
-const fov = Math.atan(0.5) * 180 / Math.PI * 2;
-const camera = new THREE.PerspectiveCamera(fov, imgWidth / imgHeight, 1, 1000); // TODO: adjust sizing
-camera.position.set(0, 0, width * 2);
+const fov = Math.atan((cubeSize + 2 * verticalPadding) / (2 * (distanceFromCube + cubeSize * 0.5))) * 180 / Math.PI * 2;
+const camera = new THREE.PerspectiveCamera(fov, imgWidth / imgHeight, 1, 1000);
+camera.position.set(0, 0, distanceFromCube + cubeSize * 0.5);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
